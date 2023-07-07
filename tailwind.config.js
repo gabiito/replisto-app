d2b04f6a2c35f89ca8bc2eb54d86f581d362d8b1
Gabriel Zerbino <gabozerbino@gmail.com>
2023-07-07 00:39:23 -0300

    Init proyect

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  corePlugins: {
+    preflight: false,
+  },
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
